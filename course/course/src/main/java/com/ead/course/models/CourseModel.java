247920ef1e8959613b7f574b371dd21367477947
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2023-11-16 07:01:35 -0300

    controllers de aulas e modulos

--- course/course/src/main/java/com/ead/course/models/CourseModel.java
+++ course/course/src/main/java/com/ead/course/models/CourseModel.java
@@ -4,7 +4,12 @@ import com.ead.course.enums.CourseLevel;
 import com.ead.course.enums.CourseStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -43,6 +48,9 @@ public class CourseModel implements Serializable {
     @Column(nullable = false)
     private UUID userInstructor;
 
-    @OneToMany(mappedBy = "course")
+    @JsonProperty(access= JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "course", fetch= FetchType.LAZY)
+    @Fetch(FetchMode.SUBSELECT)
+//    @OnDelete(action= OnDeleteAction.CASCADE)
     private Set<ModuleModel> modules;
 }
--- course/course/src/main/java/com/ead/course/models/ModuleModel.java
+++ course/course/src/main/java/com/ead/course/models/ModuleModel.java
@@ -2,11 +2,15 @@ package com.ead.course.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Data
@@ -27,6 +31,11 @@ public class ModuleModel implements Serializable {
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern= "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
 
-    @ManyToOne(optional = false)
+    @JsonProperty(access= JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne(optional = false, fetch= FetchType.LAZY)
     private CourseModel course;
+    @JsonProperty(access= JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy="module", fetch= FetchType.LAZY)
+    @Fetch(FetchMode.SUBSELECT)
+    private Set<LessonModel> lessons;
 }
